b483aeab28bc2387c18421fbd1cceecabd74089e
CodebyAnandi <anandi.tanthetaa@gmail.com>
2024-04-27 11:26:42 +0530

    code changessssssss

--- index.js
+++ index.js
@@ -4,210 +4,210 @@ const axios = require('axios');
 const express = require('express')
 const app = express();
 
-const PORT = 3000;
-console.log("🚀 ~ PORT:", PORT)
+const PORT = process.env.PORT || 3333;
 const TOKEN = process.env.TOKEN; // Telegram Token
 const API_URL = process.env.BACKEND_URL; // Backend URL
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 const buyKeyboard = {
-    inline_keyboard: [
-      [
-        { text: 'Swap Token', callback_data: 'SwaptokenButton' },
-      ],
-      [
-        { text: '🗘Refresh', callback_data: 'refreshButton' },
-        { text: '💼Balance', callback_data: 'balanceButton' },
-      ],
+  inline_keyboard: [
+    [
+      { text: 'Swap Token', callback_data: 'SwaptokenButton' },
     ],
-  };
+    [
+      { text: '🗘Refresh', callback_data: 'refreshButton' },
+      { text: '💼Balance', callback_data: 'balanceButton' },
+    ],
+  ],
+};
 
-  const blockchainKeyboard = {
-    inline_keyboard: [
-      [
-        { text: 'Ethereum', callback_data: '1' },
-        { text: 'Arbitrum', callback_data: '42161 ' },
-        { text: 'Optimism', callback_data: '10' },
-      ],
-      [
-        { text: 'Polygon', callback_data: '137' },
-        { text: 'Base', callback_data: '8453' },
-        { text: 'BNB Chain', callback_data: '56' },
-      ],
-      [
-        { text: 'Avalanche', callback_data: '43114' },
-        { text: 'Celo', callback_data: '42220' },
-        { text: 'Blast', callback_data: '238' },
-      ],
+const blockchainKeyboard = {
+  inline_keyboard: [
+    [
+      { text: 'Ethereum', callback_data: '1' },
+      { text: 'Arbitrum', callback_data: '42161 ' },
+      { text: 'Optimism', callback_data: '10' },
     ],
-  };
+    [
+      { text: 'Polygon', callback_data: '137' },
+      { text: 'Base', callback_data: '8453' },
+      { text: 'BNB Chain', callback_data: '56' },
+    ],
+    [
+      { text: 'Avalanche', callback_data: '43114' },
+      { text: 'Celo', callback_data: '42220' },
+      { text: 'Blast', callback_data: '238' },
+    ],
+  ],
+};
 
 
 // Event listener for handling messages
 bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-    if (msg.text === '/start') {
-        sendWelcomeMessage(chatId);
-        bot.sendMessage(chatId, messageText, { reply_markup: JSON.stringify(buyKeyboard) });
-    } else if (msg.text === 'SignUp') {
-        startSignUp(chatId);
-    } else if (msg.text === 'Login') {
-        startLogin(chatId);
-    } else if (msg.text === 'SwaptokenButton') {
-        startSwapToken(chatId);
-    } else {
-        bot.sendMessage(chatId, `You typed: ${msg.text}`);
-    }
+  if (msg.text === '/start') {
+    sendWelcomeMessage(chatId);
+    bot.sendMessage(chatId, messageText, { reply_markup: JSON.stringify(buyKeyboard) });
+  } else if (msg.text === 'SignUp') {
+    startSignUp(chatId);
+  } else if (msg.text === 'Login') {
+    startLogin(chatId);
+  } else if (msg.text === 'SwaptokenButton') {
+    bot.sendMessage(chatId, messageText, { reply_markup: JSON.stringify(blockchainKeyboard) });
+    startSwapToken(chatId);
+  } else {
+    bot.sendMessage(chatId, `You typed: ${msg.text}`);
+  }
 });
 
 // Function to send the welcome message
 function sendWelcomeMessage(chatId) {
-    bot.sendMessage(chatId, '👋 Welcome to the bot! Type something in the textbox:', {
-        reply_markup: {
-            keyboard: [
-                [{ text: 'SignUp', request_contact: false, request_location: false }],
-                // [{ text: 'SwapToken', request_contact: false, request_location: false }],
-                [{ text: 'Login', request_contact: false, request_location: false }],
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: true,
-        },
-    });
+  bot.sendMessage(chatId, '👋 Welcome to the bot! Type something in the textbox:', {
+    reply_markup: {
+      keyboard: [
+        [{ text: 'SignUp', request_contact: false, request_location: false }],
+        // [{ text: 'SwapToken', request_contact: false, request_location: false }],
+        [{ text: 'Login', request_contact: false, request_location: false }],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    },
+  });
 }
 
 function startSignUp(chatId) {
-    bot.sendMessage(chatId, '👋 Welcome! Please provide your name:');
-    bot.once('message', async (nameMsg) => {
-        const name = nameMsg.text;
-        console.log("🚀 ~ bot.once ~ name:", name)
-        bot.sendMessage(chatId, `Great, thanks ${name}! Next, please provide your email address:`);
-        bot.once('message', async (emailMsg) => {
-            const email = emailMsg.text;
-            console.log("🚀 ~ bot.once ~ email:", email)
-            bot.sendMessage(chatId, 'Awesome! Now, please create a password:');
-            bot.once('message', async (passwordMsg) => {
-                const password = passwordMsg.text;
-                bot.sendMessage(chatId, 'Got it! Please confirm your password:');
-                bot.once('message', async (confirmPasswordMsg) => {
-                    const confirmPassword = confirmPasswordMsg.text;
-                    try {
-                        const response = await axios.post(`${API_URL}/signup`, {
-                            name,
-                            email,
-                            password,
-                            confirmPassword,
-                            chatId
-                        });
-                        const { message, data } = response.data;
-                        if (data && data.email) {
-                            await bot.sendMessage(chatId, `🎉 User registered successfully. Email: ${data.email}`);
-                            bot.sendMessage(chatId, '📧 Please check your email for a verification code:');
-                            bot.once('message', async (otpMsg) => {
-                                const otp = otpMsg.text;
-                                try {
-                                    const response = await axios.post(`${API_URL}/verify`, {
-                                        email,
-                                        otp,
-                                    });
-                                    if (response.data.status === true) {
-                                        await bot.sendMessage(chatId, `✅ User verified successfully`);
-                                    } else if (response.data.status === false) {
-                                        bot.sendMessage(chatId, `❌ Invalid OTP. Please enter a valid OTP.`);
-                                    }
-                                } catch (error) {
-                                    console.error('Error:', error.message);
-                                    bot.sendMessage(chatId, `❌ An error occurred while verifying the user: ${error.message}`);
-                                }
-                            });
-                        } else {
-                            bot.sendMessage(chatId, `❌ Failed to register user. Please try again.`);
-                        }
-                    } catch (error) {
-                        console.error('Error:', error.message);
-                        bot.sendMessage(chatId, `❌ An error occurred while registering the user: ${error.message}`);
-                    }
-                });
+  bot.sendMessage(chatId, '👋 Welcome! Please provide your name:');
+  bot.once('message', async (nameMsg) => {
+    const name = nameMsg.text;
+    console.log("🚀 ~ bot.once ~ name:", name)
+    bot.sendMessage(chatId, `Great, thanks ${name}! Next, please provide your email address:`);
+    bot.once('message', async (emailMsg) => {
+      const email = emailMsg.text;
+      console.log("🚀 ~ bot.once ~ email:", email)
+      bot.sendMessage(chatId, 'Awesome! Now, please create a password:');
+      bot.once('message', async (passwordMsg) => {
+        const password = passwordMsg.text;
+        bot.sendMessage(chatId, 'Got it! Please confirm your password:');
+        bot.once('message', async (confirmPasswordMsg) => {
+          const confirmPassword = confirmPasswordMsg.text;
+          try {
+            const response = await axios.post(`${API_URL}/signup`, {
+              name,
+              email,
+              password,
+              confirmPassword,
+              chatId
             });
+            const { message, data } = response.data;
+            if (data && data.email) {
+              await bot.sendMessage(chatId, `🎉 User registered successfully. Email: ${data.email}`);
+              bot.sendMessage(chatId, '📧 Please check your email for a verification code:');
+              bot.once('message', async (otpMsg) => {
+                const otp = otpMsg.text;
+                try {
+                  const response = await axios.post(`${API_URL}/verify`, {
+                    email,
+                    otp,
+                  });
+                  if (response.data.status === true) {
+                    await bot.sendMessage(chatId, `✅ User verified successfully`);
+                  } else if (response.data.status === false) {
+                    bot.sendMessage(chatId, `❌ Invalid OTP. Please enter a valid OTP.`);
+                  }
+                } catch (error) {
+                  console.error('Error:', error.message);
+                  bot.sendMessage(chatId, `❌ An error occurred while verifying the user: ${error.message}`);
+                }
+              });
+            } else {
+              bot.sendMessage(chatId, `❌ Failed to register user. Please try again.`);
+            }
+          } catch (error) {
+            console.error('Error:', error.message);
+            bot.sendMessage(chatId, `❌ An error occurred while registering the user: ${error.message}`);
+          }
         });
+      });
     });
+  });
 }
 
 function startLogin(chatId) {
-    bot.sendMessage(chatId, '🔐 Please enter your email to log in:');
-    bot.once('message', async (emailMsg) => {
-        const email = emailMsg.text;
-        bot.sendMessage(chatId, '🔑 Now, please enter your password:');
-        bot.once('message', async (passwordMsg) => {
-            const password = passwordMsg.text;
-            try {
-                const response = await axios.post(`${API_URL}/login`, {
-                    email,
-                    password,
-                    chatId,
-                });
-                if (response.data.status === true) {
-                    bot.sendMessage(chatId, `✅ Login successful!`);
-                } else {
-                    bot.sendMessage(chatId, '❌ Invalid email or password. Please try again.');
-                }
-            } catch (error) {
-                console.error('Error:', error.message);
-                bot.sendMessage(chatId, `❌ An error occurred while logging in: ${error.message}`);
-            }
+  bot.sendMessage(chatId, '🔐 Please enter your email to log in:');
+  bot.once('message', async (emailMsg) => {
+    const email = emailMsg.text;
+    bot.sendMessage(chatId, '🔑 Now, please enter your password:');
+    bot.once('message', async (passwordMsg) => {
+      const password = passwordMsg.text;
+      try {
+        const response = await axios.post(`${API_URL}/login`, {
+          email,
+          password,
+          chatId,
         });
+        if (response.data.status === true) {
+          bot.sendMessage(chatId, `✅ Login successful!`);
+        } else {
+          bot.sendMessage(chatId, '❌ Invalid email or password. Please try again.');
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+        bot.sendMessage(chatId, `❌ An error occurred while logging in: ${error.message}`);
+      }
     });
+  });
 
 }
 
 function startSwapToken(chatId) {
-    bot.sendMessage(chatId, ' Please enter the chain ID:');
-    bot.once('message', async (chainIdMsg) => {
-      const chainId = Number(chainIdMsg.text);
-      if (isNaN(chainId)) {
-        return bot.sendMessage(chatId, '❌ Invalid chain ID. Please enter a valid number.');
-      }
-      bot.sendMessage(chatId, ' Please enter the first token:');
-      bot.once('message', async (token0Msg) => {
-        const token0 = token0Msg.text;
+  bot.sendMessage(chatId, ' Please enter the chain ID:');
+  bot.once('message', async (chainIdMsg) => {
+    const chainId = Number(chainIdMsg.text);
+    if (isNaN(chainId)) {
+      return bot.sendMessage(chatId, '❌ Invalid chain ID. Please enter a valid number.');
+    }
+    bot.sendMessage(chatId, ' Please enter the first token:');
+    bot.once('message', async (token0Msg) => {
+      const token0 = token0Msg.text;
 
-        bot.sendMessage(chatId, ' Please enter the second token:');
-        bot.once('message', async (token1Msg) => {
-          const token1 = token1Msg.text;
-  
-          bot.sendMessage(chatId, ' Please enter the amount to swap:');
-          bot.once('message', async (amountInMsg) => {
-            const amountIn = Number(amountInMsg.text);
-  
-            if (isNaN(amountIn)) {
-              return bot.sendMessage(chatId, '❌ Invalid amount. Please enter a valid number.');
-            }
+      bot.sendMessage(chatId, ' Please enter the second token:');
+      bot.once('message', async (token1Msg) => {
+        const token1 = token1Msg.text;
 
-            try {
-                const response = await axios.post(`${API_URL}/mainswap`, {
-                    token0,
-                    token1,
-                    amountIn,
-                    chainId,
-                    chatId,
-                });
-                
-              if (response.data.status === true) {
-                bot.sendMessage(chatId, `Swap successful!`);
-              } else {
-                bot.sendMessage(chatId, response.data.message || '❌ Swap failed. Please try again.');
-              }
-            } catch (error) {
-              bot.sendMessage(chatId, `❌ An error occurred: ${error.message}`); // Provide more specific error message if possible
+        bot.sendMessage(chatId, ' Please enter the amount to swap:');
+        bot.once('message', async (amountInMsg) => {
+          const amountIn = Number(amountInMsg.text);
+
+          if (isNaN(amountIn)) {
+            return bot.sendMessage(chatId, '❌ Invalid amount. Please enter a valid number.');
+          }
+
+          try {
+            const response = await axios.post(`${API_URL}/mainswap`, {
+              token0,
+              token1,
+              amountIn,
+              chainId,
+              chatId,
+            });
+
+            if (response.data.status === true) {
+              bot.sendMessage(chatId, `Swap successful!`);
+            } else {
+              bot.sendMessage(chatId, response.data.message || '❌ Swap failed. Please try again.');
             }
-          });
+          } catch (error) {
+            bot.sendMessage(chatId, `❌ An error occurred: ${error.message}`); // Provide more specific error message if possible
+          }
         });
       });
     });
-  }
+  });
+}
 
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${PORT}`);
+  console.log(`Our app is running on port ${PORT}`);
 });
 console.log('Bot started!');
